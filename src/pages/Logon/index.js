e90df08589abf15df7c53d36b721e47573c31718
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-03-28 23:19:53 -0300

    feat(logon): add icon with router-icons module

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -14,9 +15,10 @@ export default function Logon() {
         <form>
           <h1>Faça seu logon</h1>
           <input placeholder="Sua ID"/>
-          <button type="submit">Entrar</button>
+          <button className="button" type="submit">Entrar</button>
 
           <a href="/register">
+            <FiLogIn size={16} color="#E02041" />
             Não tenho cadastro
           </a>
         </form>
